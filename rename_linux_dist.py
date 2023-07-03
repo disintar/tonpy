d715f01eb674628c997248a5fae9198da3bce0dd
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-03 21:50:20 +0300

    Fix dist folder

--- rename_linux_dist.py
+++ rename_linux_dist.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     dst = os.listdir('dist')
     for file in dst:
         if 'linux_x86_64' in file:
-            os.rename(file, file.replace("linux_x86_64", "manylinux2014_x86_64"))
+            os.rename(f"dist/{file}", f'dist/{file.replace("linux_x86_64", "manylinux2014_x86_64")}')
