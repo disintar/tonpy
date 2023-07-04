865b1e64bf089ff256bfc8077f436912672da44f
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-04 10:42:00 +0300

    Fix replace tag

--- fix_whl_name.py
+++ fix_whl_name.py
@@ -3,5 +3,6 @@ import os
 if __name__ == "__main__":
     dst = os.listdir('dist')
     for file in dst:
-        if 'linux_x86_64' in file or 'universal2' in file:
-            os.rename(f"dist/{file}", f'dist/{file.replace("universal2", os.getenv("TAG_FIX"))}')
+        for tag in ['linux_x86_64', 'universal2']:
+            if tag in file:
+                os.rename(f"dist/{file}", f'dist/{file.replace(tag, os.getenv("TAG_FIX"))}')
