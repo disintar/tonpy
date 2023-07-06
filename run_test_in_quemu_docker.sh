ddfb432f23fbcfa92bce6eec07c98b11d11c3b66
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-06 12:27:03 +0300

    Add quemu tests on arm64 linux

---
+++ run_test_in_quemu_docker.sh
@@ -0,0 +1,4 @@
+python -m pip install -r /tmp/tonpy/built_requirements.txt
+python -m pip install -r /tmp/tonpy/docs_requirements.txt
+python -m pip install -r /tmp/tonpy/requirements.txt
+cd /tmp/tonpy && pytest
