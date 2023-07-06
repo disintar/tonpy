942571e02760d2161d52a8e071b15767f97342c1
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-06 13:47:42 +0300

    Fix sh

--- run_test_in_quemu_docker.sh
+++ run_test_in_quemu_docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 python -m pip install -r /tmp/tonpy/built_requirements.txt
 python -m pip install -r /tmp/tonpy/docs_requirements.txt
 python -m pip install -r /tmp/tonpy/requirements.txt
