cb37e705e54083ea99ac64489d87c67da077cb9e
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-06 15:13:56 +0300

    Force ip

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+echo """
+151.101.0.223 pypi.org
+151.101.1.63 files.pythonhosted.org
+""" >> /etc/hosts
+
 python -m pip install --default-timeout=100 --upgrade pip
 python -m pip install --default-timeout=100 -r /tmp/tonpy/requirements.txt
 python -m pip install --default-timeout=100 -r /tmp/tonpy/built_requirements.txt
