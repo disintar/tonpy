52a8f71c3fdbe255d6282428e0638b08ab3c0686
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-06 15:02:09 +0300

    Move macos&linux back

--- fix_whl_name.py
+++ fix_whl_name.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
     dst = os.listdir('dist')
     for file in dst:
-        for tag in ['linux_x86_64', 'universal2']:
+        for tag in ['linux_x86_64', 'universal2', 'linux_aarch64']:
             if tag in file:
                 os.rename(f"dist/{file}", f'dist/{file.replace(tag, os.getenv("TAG_FIX"))}')
--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python -m pip install -r /tmp/tonpy/requirements.txt
-python -m pip install -r /tmp/tonpy/built_requirements.txt
+python -m pip install --default-timeout=100 --upgrade pip
+python -m pip install --default-timeout=100 -r /tmp/tonpy/requirements.txt
+python -m pip install --default-timeout=100 -r /tmp/tonpy/built_requirements.txt
 cd /tmp/tonpy && pytest
 cd /tmp/tonpy && python -m build -n --wheel --outdir dist/ .
 cd /tmp/tonpy && python fix_whl_name.py
