c2876ef03d00d8dd538e6c12c34d15549515221a
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-06 14:52:14 +0300

    Fix isolation

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -2,5 +2,5 @@
 python -m pip install -r /tmp/tonpy/requirements.txt
 python -m pip install -r /tmp/tonpy/built_requirements.txt
 cd /tmp/tonpy && pytest
-cd /tmp/tonpy && python -m build --wheel --outdir dist/ .
+cd /tmp/tonpy && python -m build -n --wheel --outdir dist/ .
 cd /tmp/tonpy && python fix_whl_name.py
