98da7f8533d2d757fb09cd3f4bac1378fe58b3ec
Andrey Tvorozhkov <andrey@head-labs.com>
2023-07-06 13:59:28 +0300

    Separate virtual

--- run_test_in_quemu_docker.sh
+++ run_test_in_quemu_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-python -m pip install -r /tmp/tonpy/built_requirements.txt
-python -m pip install -r /tmp/tonpy/docs_requirements.txt
-python -m pip install -r /tmp/tonpy/requirements.txt
+/tmp/tonpy/install_prebuilt.sh
+python -m pip --default-timeout=1000 install -r /tmp/tonpy/built_requirements.txt
+python -m pip --default-timeout=1000 install -r /tmp/tonpy/docs_requirements.txt
+python -m pip --default-timeout=1000 install -r /tmp/tonpy/requirements.txt
 cd /tmp/tonpy && pytest
+cd /tmp/tonpy && python -m build --wheel --outdir dist/ .
+cd /tmp/tonpy && python fix_whl_name.py
