9dc48b78d2571b68dc46bcafcbe93cb68ad220cf
Andrey Tvorozhkov <andrey@head-labs.com>
2023-09-22 20:40:41 +0300

    Fix set_gas_limit for TVM

--- src/tonpy/tvm/__init__.py
+++ src/tonpy/tvm/__init__.py
@@ -1,2 +1,2 @@
-from tonpy.tvm.tvm import TVM, method_name_to_id
+from tonpy.tvm.tvm import TVM, method_name_to_id, C7
 from tonpy.tvm.emulator import Emulator
--- src/tonpy/utils/token.py
+++ src/tonpy/utils/token.py
@@ -1,6 +1,10 @@
 from tonpy.libs.python_ton import parse_token_data as parse_token_data_cell
-from tonpy.types import Cell
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tonpy import Cell
 
 
-def parse_token_data(cell: Cell):
+def parse_token_data(cell: "Cell"):
     return parse_token_data(cell.cell)
