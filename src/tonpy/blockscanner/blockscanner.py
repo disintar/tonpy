2b3ec01e9fb05ead5da97104024cb3b82fa277d1
Andrey Tvorozhkov <andrey@head-labs.com>
2024-04-20 15:12:59 +0400

    Move back working code

--- src/tonpy/blockscanner/blockscanner.py
+++ src/tonpy/blockscanner/blockscanner.py
@@ -460,8 +460,8 @@ def process_mc_blocks(seqnos, lcparams, loglevel, parse_txs_over_ls):
             if loglevel > 3:
                 logger.debug(f"[mc t{thread_id}] Stop liteclient")
 
-            if loglevel > 2:
-                logger.debug(f"[mc t{thread_id}] Done load MCs")
+        if loglevel > 2:
+            logger.debug(f"[mc t{thread_id}] Done load MCs")
 
         return answer
     except Exception as e:
@@ -579,22 +579,19 @@ class BlockScanner(Thread):
         blocks_ids = list(range(from_, to_))
         mc_seqnos_chunks, p = self.detect_cs_p(blocks_ids)
 
-        # with Pool(p) as pool:
-        #     results = pool.imap_unordered(process_mc_blocks(lcparams=self.lcparams, loglevel=self.loglevel,
-        #                                                     parse_txs_over_ls=self.parse_txs_over_ls),
-        #                                   enumerate(mc_seqnos_chunks))
-        #
-        #     if self.loglevel > 1:
-        #         results = tqdm(results, desc="Download MC blocks", total=len(mc_seqnos_chunks))
-        #
-        #     for result in results:
-        #         if result is None:
-        #             raise ValueError(f"Invalid result in MC blocks")
-        #         mc_data.extend(result)
+        with Pool(p) as pool:
+            results = pool.imap_unordered(process_mc_blocks(lcparams=self.lcparams, loglevel=self.loglevel,
+                                                            parse_txs_over_ls=self.parse_txs_over_ls),
+                                          enumerate(mc_seqnos_chunks))
 
-        for i in enumerate(mc_seqnos_chunks):
-            mc_data.extend(process_mc_blocks(lcparams=self.lcparams, loglevel=self.loglevel,
-                                             parse_txs_over_ls=self.parse_txs_over_ls, seqnos=i))
+            if self.loglevel > 1:
+                results = tqdm(results, desc="Download MC blocks", total=len(mc_seqnos_chunks))
+
+            for result in results:
+                if result is None:
+                    raise ValueError(f"Invalid result in MC blocks")
+                mc_data.extend(result)
+
         # todo: check hashes
         return mc_data
 
